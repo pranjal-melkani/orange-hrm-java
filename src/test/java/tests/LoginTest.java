41b17742b77acf3df5a93e14100c782396d9f377
Pranjal <pranjal.melkani18@gmail.com>
2025-08-24 17:29:25 +0530

    Valid login test updated

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.Basetest;
+import pages.DashboardPage;
 import pages.LoginPage;
 
 public class LoginTest extends Basetest {
@@ -12,5 +13,7 @@ public class LoginTest extends Basetest {
 		LoginPage lp = new LoginPage(driver);
 		lp.login("Admin", "admin123");
 		Assert.assertTrue(lp.is_loggedIn(), "User is not logged in");
+		DashboardPage dp = new DashboardPage(driver);
+		Assert.assertTrue(dp.dashboard_isVisible(), "User is not logged in");
 	}
 }
