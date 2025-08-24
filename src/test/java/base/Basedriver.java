de007a32d35f437e1093303a3d629931c86ce796
Pranjal <pranjal.melkani18@gmail.com>
2025-08-24 12:25:59 +0530

    Basedriver added

---
+++ src/test/java/base/Basedriver.java
@@ -0,0 +1,34 @@
+package base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Basedriver {
+	WebDriver driver;
+	
+	public Basedriver(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void wait_until_element_is_visible(By locator) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+	}
+	
+	public void click_on_element(By locator) {
+		wait_until_element_is_visible(locator);
+		WebElement element = driver.findElement(locator);
+		element.click();
+	}
+	
+	public void send_keys(By locator, String input_text) {
+		wait_until_element_is_visible(locator);
+		WebElement element = driver.findElement(locator);
+		element.sendKeys(input_text);
+	}
+}
