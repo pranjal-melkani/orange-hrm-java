01988f183631f746d0b0b5f29389fad72e4d5a8c
Pranjal <pranjal.melkani18@gmail.com>
2025-08-24 12:19:14 +0530

    Basetest - setup/teardown

---
+++ src/test/java/base/Basetest.java
@@ -0,0 +1,41 @@
+package base;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ReadConfig;
+
+public class Basetest {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() throws IOException {
+		ReadConfig config = new ReadConfig();
+		String browser = config.getProperty("browser");
+		String url = config.getProperty("url");
+
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.get(url);
+		driver.manage().window().maximize();
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+}
