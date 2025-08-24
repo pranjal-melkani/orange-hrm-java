c19fe5e6e8986e8af8637ffbc5afa732e0a711a6
Pranjal <pranjal.melkani18@gmail.com>
2025-08-24 13:28:42 +0530

    Login page locators added

---
+++ src/test/java/locators/LoginPageLocators.java
@@ -0,0 +1,9 @@
+package locators;
+
+import org.openqa.selenium.By;
+
+public class LoginPageLocators {
+	public static final By USERNAME_FIELD = By.name("username");
+	public static final By PASSWORD_FIELD = By.name("password");
+	public static final By LOGIN_BTN = By.tagName("button");
+}
