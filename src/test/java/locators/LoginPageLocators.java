814424add115960fab3c5440e23cd100992a1570
Pranjal <pranjal.melkani18@gmail.com>
2025-08-24 18:00:43 +0530

    Invalid credentials error locator added

--- src/test/java/locators/LoginPageLocators.java
+++ src/test/java/locators/LoginPageLocators.java
@@ -6,4 +6,6 @@ public class LoginPageLocators {
 	public static final By USERNAME_FIELD = By.name("username");
 	public static final By PASSWORD_FIELD = By.name("password");
 	public static final By LOGIN_BTN = By.tagName("button");
+	public static final By INVALID_CREDENTIALS_ERROR = By
+			.xpath("//div[@class='orangehrm-login-error']//p[text()='Invalid credentials']");
 }
